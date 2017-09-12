e07863d65c6389aa671c2f2dadb5cc9afdec592f
David Gassner <david.gassner@gmail.com>
2017-09-12 13:26:46 -0700

    Initial commit

---
+++ app/src/main/java/com/example/android/concurrency/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.android.concurrency;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "CodeRunner";
+
+    // View object references
+    private ScrollView mScroll;
+    private TextView mLog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize the logging components
+        mScroll = (ScrollView) findViewById(R.id.scrollLog);
+        mLog = (TextView) findViewById(R.id.tvLog);
+        mLog.setText(R.string.lorem_ipsum);
+    }
+
+    //  Run some code, called from the onClick event in the layout file
+    public void runCode(View v) {
+        log("Running code");
+    }
+
+    //  Clear the output, called from the onClick event in the layout file
+    public void clearOutput(View v) {
+        mLog.setText("");
+        scrollTextToEnd();
+    }
+
+    //  Log output to logcat and the screen
+    private void log(String message) {
+        Log.i(TAG, message);
+        mLog.append(message + "\n");
+        scrollTextToEnd();
+    }
+
+    private void scrollTextToEnd() {
+        mScroll.post(new Runnable() {
+            @Override
+            public void run() {
+                mScroll.fullScroll(ScrollView.FOCUS_DOWN);
+            }
+        });
+    }
+}
