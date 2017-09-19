6c268950e7b5b1ebb9cb2a53194870345baca271
David Gassner <dgassnerdev@gmail.com>
2017-09-19 10:55:03 -0700

    03_03_complete

---
+++ app/src/main/java/com/example/android/concurrency/MyService.java
@@ -0,0 +1,47 @@
+package com.example.android.concurrency;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+
+    public static final String TAG = "CodeRunner";
+    private final Binder mBinder = new ServiceBinder();
+
+    public MyService() {
+        Log.i(TAG, "MyService: service created");
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.i(TAG, "onBind");
+        return mBinder;
+    }
+
+    class ServiceBinder extends Binder {
+        MyService getService() {
+            return MyService.this;
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(TAG, "onCreate");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.i(TAG, "onUnbind: service");
+        return super.onUnbind(intent);
+    }
+}
