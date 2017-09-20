4dca3fdf434150bc69a412b6ec9a14065152bc41
David Gassner <dgassnerdev@gmail.com>
2017-09-20 12:34:48 -0700

    05_01

--- app/src/main/java/com/example/android/concurrency/MainActivity.java
+++ app/src/main/java/com/example/android/concurrency/MainActivity.java
@@ -2,20 +2,14 @@ package com.example.android.concurrency;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.ProgressBar;
-import android.widget.ScrollView;
-import android.widget.TextView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "CodeRunner";
 
     // View object references
-    private ScrollView mScroll;
-    private TextView mLog;
-    private ProgressBar mProgressBar;
+    ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,46 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Initialize the logging components
-        mScroll = (ScrollView) findViewById(R.id.scrollLog);
-        mLog = (TextView) findViewById(R.id.tvLog);
-        mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
-
-        mLog.setText(R.string.lorem_ipsum);
+        mListView = (ListView) findViewById(R.id.list_view);
     }
 
-    //  Run some code, called from the onClick event in the layout file
-    public void runCode(View v) {
-        log("Running code");
-    }
-
-    //  Clear the output, called from the onClick event in the layout file
-    public void clearOutput(View v) {
-        mLog.setText("");
-        scrollTextToEnd();
-    }
-
-    //  Log output to logcat and the screen
-    private void log(String message) {
-        Log.i(TAG, message);
-        mLog.append(message + "\n");
-        scrollTextToEnd();
-    }
-
-    private void scrollTextToEnd() {
-        mScroll.post(new Runnable() {
-            @Override
-            public void run() {
-                mScroll.fullScroll(ScrollView.FOCUS_DOWN);
-            }
-        });
-    }
-
-    @SuppressWarnings("unused")
-    private void displayProgressBar(boolean display) {
-        if (display) {
-            mProgressBar.setVisibility(View.VISIBLE);
-        } else {
-            mProgressBar.setVisibility(View.INVISIBLE);
-        }
-    }
 }
