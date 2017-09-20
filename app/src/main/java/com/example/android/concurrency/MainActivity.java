6bb7fd48b6a67bf73a02ac9769dda2a390e69ecf
David Gassner <dgassnerdev@gmail.com>
2017-09-20 12:45:00 -0700

    05_01

--- app/src/main/java/com/example/android/concurrency/MainActivity.java
+++ app/src/main/java/com/example/android/concurrency/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.android.concurrency;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "CodeRunner";
+    private static final int REQUEST_CODE_PERMISSION = 1001;
 
     // View object references
-    ListView mListView;
+    private ListView mListView;
+    private boolean mPermissionGranted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,33 @@ public class MainActivity extends AppCompatActivity {
 
         // Initialize the logging components
         mListView = (ListView) findViewById(R.id.list_view);
+
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS);
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            mPermissionGranted = true;
+            Toast.makeText(this, "Permission already granted", Toast.LENGTH_SHORT).show();
+            loadContactsData();
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                this.requestPermissions(new String[]{Manifest.permission.READ_CONTACTS},
+                        REQUEST_CODE_PERMISSION);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_PERMISSION &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+            mPermissionGranted = true;
+            loadContactsData();
+        }
+    }
+
+    private void loadContactsData() {
     }
 
 }
