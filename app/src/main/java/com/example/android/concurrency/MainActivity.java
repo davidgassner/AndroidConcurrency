d46d4b910c406221c88f2fb7cf4eba0a6d890285
Michael Balling <mballing@students.mchenry.edu>
2020-03-17 14:41:27 -0500

    Finished Section 2.0

--- app/src/main/java/com/example/android/concurrency/MainActivity.java
+++ app/src/main/java/com/example/android/concurrency/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.concurrency;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -33,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
 
     //  Run some code, called from the onClick event in the layout file
     public void runCode(View v) {
+        MyTask task = new MyTask();
+        task.execute("Red", "Green", "Blue");
+        MyTask task2 = new MyTask();
+        task2.execute("Pink", "Orange", "Purple");
     }
 
     //  Clear the output, called from the onClick event in the layout file
@@ -47,6 +52,23 @@ public class MainActivity extends AppCompatActivity {
         Log.i(TAG, message);
         mLog.append(message + "\n");
         scrollTextToEnd();
+    }
+
+    //TODO: when declaring the AsyncTask we pass in three types as generic declarations, first: parameter, second: progress, third: result
+    class MyTask extends AsyncTask<String, String, String> {
+        @Override
+        //TODO: code executes in a background thread, this cannot complete tasks that excute in the Main Thread
+        protected String doInBackground(String... strings) {
+            for (String value: strings) {
+                Log.i(TAG, "doInBackground: " + value);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
     }
 
     private void scrollTextToEnd() {
