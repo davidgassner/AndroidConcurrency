6ffd3f358591fd2e85139b7d3e8b90e8bc401102
Michael Balling <mballing@students.mchenry.edu>
2020-03-17 12:58:06 -0500

    Finished Section 1.3

--- app/src/main/java/com/example/android/concurrency/MainActivity.java
+++ app/src/main/java/com/example/android/concurrency/MainActivity.java
@@ -39,12 +39,20 @@ public class MainActivity extends AppCompatActivity {
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                Log.i(TAG, "run: runnable complete");
-                displayProgressBar(false);
+                Log.i(TAG, "run: starting thread for 4 seconds");
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+//                displayProgressBar(false);
             }
         };
-        Handler handler = new Handler();
-        handler.postDelayed(runnable, 3000);
+
+        Thread thread = new Thread(runnable);
+        thread.start();
+//        Handler handler = new Handler();
+//        handler.postDelayed(runnable, 3000);
 
     }
 
